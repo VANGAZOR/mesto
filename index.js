c55fda8f3a6cfbc3f0b8c8bda3648c14a3e0f188
VANGAZOR <asei3k@gmail.com>
2022-12-21 10:27:23 +0300

    исправлены ошибки после 1 ревью

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-let btn = document.querySelector('.profile__edit-button')
-let btn_close = document.querySelector('.popup__exit-button')
-let popup_cont = document.querySelector('.popup__container')
-let main = document.querySelector('.main')
-let wrap = document.querySelector('.wrap')
-let popup_form = document.querySelector('.popup__form')
-let like_btn = document.querySelector('.elements__like-button')
-let like_btn_active = document.querySelector('.elements_like-button_active')
-let form = document.querySelector('.popup__form')
-let nameForm = document.querySelector('.popup__name')
-let subNameForm = document.querySelector('.popup__subname')
+const btnEdit = document.querySelector('.profile__edit-button')
+const btnClose = document.querySelector('.popup__exit-button')
+const popupOn = document.querySelector('.popup')
+const like_btn = document.querySelector('.elements__like-button')
+const like_btn_active = document.querySelector('.elements_like-button_active')
+const form = document.querySelector('.popup__form')
+const nameForm = document.querySelector('.popup__name')
+const subNameForm = document.querySelector('.popup__subname')
 const title = document.querySelector('.profile__title')
 const subTitle = document.querySelector('.profile__subtitle')
 
@@ -18,8 +15,7 @@ function handleFormSubmit (evt) {
     subNameValue = subNameForm.value
     title.textContent = nameValue
     subTitle.textContent = subNameValue
-    wrap.classList.remove('popup__active')
-    popup_cont.classList.remove('popup__container_active')
+    popupOn.classList.remove('popup__active')
 }
 form.addEventListener('submit', handleFormSubmit); 
 
@@ -33,14 +29,12 @@ like_btn.addEventListener('click', function () {
     }
 })
 
-btn.addEventListener('click', function () {
-    wrap.classList.add('popup__active')
-    popup_cont.classList.add('popup__container_active')
+btnEdit.addEventListener('click', function () {
+    popupOn.classList.add('popup__active')
     nameForm.value = title.innerHTML
 })
 
-btn_close.addEventListener('click', function () {
-    wrap.classList.remove('popup__active')
-    popup_cont.classList.remove('popup__container_active')
+btnClose.addEventListener('click', function () {
+    popupOn.classList.remove('popup__active')
 })
 
